94713a2e9f93ea3691d47f16a864ffa51978135b
rustam <rustam@nodasoft.com>
2014-06-11 13:53:34 +0400

    телефоны и js

--- apps/front/modules/main/templates/_header.php
+++ apps/front/modules/main/templates/_header.php
@@ -2,6 +2,10 @@
 
 echo _open('ul.headerPhone');
 	echo _tag('li', _media('headerPhoneLogo.png'));
-	echo _tag('li', _tag('h4', '+79283481501'));
+	echo _tag('li', _tag('h4', '8(87937) 7-09-65'));
+	echo _tag('br');
+	echo _tag('li', _tag('h4', '+7(928) 348-15-01'));
+	echo _tag('br');
+	echo _tag('li', _tag('h4', '+7(938) 301-89-69'));
 echo _close('ul');
 
